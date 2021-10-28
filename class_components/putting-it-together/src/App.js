1a5570432ad5193205886dcc061d382ad41ade0b
Chris Frederick <chrisfrederick2008@gmail.com>
2021-10-28 14:54:39 -0500

    putting it together

--- class_components/putting-it-together/src/App.js
+++ class_components/putting-it-together/src/App.js
@@ -4,26 +4,8 @@ import PropItUp from './Components/PropItUp'
 function App() {
   return (
     <div className="App">
-      <PropItUp name={"Doe, Jane"}/>
-      <PropItUp age={"Age"}/>
-      <PropItUp ageNum={"50"}/>
-      <PropItUp hair={"Hair Color"}/>
-      <PropItUp hairColor={"Black"}/>
-      <PropItUp name={"Smith, John"}/>
-      <PropItUp age={"Age"}/>
-      <PropItUp ageNum={"30"}/>
-      <PropItUp hair={"Hair Color"}/>
-      <PropItUp hairColor={"Red"}/>
-      <PropItUp name={"Fillmore, Millard"}/>
-      <PropItUp age={"Age"}/>
-      <PropItUp ageNum={"33"}/>
-      <PropItUp hair={"Hair Color"}/>
-      <PropItUp hairColor={"Brown"}/>
-      <PropItUp name={"Smith, Maria"}/>
-      <PropItUp age={"Age"}/>
-      <PropItUp ageNum={"15"}/>
-      <PropItUp hair={"Hair Color"}/>
-      <PropItUp hairColor={"White"}/>
+      <PropItUp lastName="Doe" firstName="Jane" age={67}/>
+      <PropItUp lastName="Miller" firstName="Frank" age={41}/>
     </div>
   );
 }
--- class_components/putting-it-together/src/Components/PropItUp.js
+++ class_components/putting-it-together/src/Components/PropItUp.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react'
 
 class PropItUp extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age
+        }
+    }
+
+
+
     render(){
-        return <div>
-            <table>
-                <th>{this.props.name}</th>
-                <tr>
-                <th>{this.props.age}</th>
-                <th>{this.props.hair}</th>
-                </tr>
-                <tr>
-                    <td>{this.props.ageNum}</td>
-                    <td>{this.props.hairColor}</td>
-                </tr>
-            </table>
+        const {firstName, lastName, age, hairColor} = this.props;
+        return ( 
+            <div>
+                <h1>{lastName}, {firstName}</h1>
+                <p>Age: {this.state.age}</p>
+                <p>HairColor: {hairColor}</p>
+                <button onClick={this.birthday}>Increase age for {firstName} {lastName}</button>
+            </div>
+        );
+    }
 
-
-        </div>
+    birthday = () => {
+        this.setState({age: this.state.age + 1})
     }
 }
 
