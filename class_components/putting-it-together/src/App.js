309f6521cb5b298c48a7493907b51f573e7cad18
Chris Frederick <chrisfrederick2008@gmail.com>
2021-10-29 12:32:45 -0500

    big inverison

--- class_components/putting-it-together/src/App.js
+++ class_components/putting-it-together/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import PropItUp from './Components/PropItUp'
+import PersonCard from './Components/PersonCard';
 
 function App() {
   return (
     <div className="App">
-      <PropItUp lastName="Doe" firstName="Jane" age={67}/>
-      <PropItUp lastName="Miller" firstName="Frank" age={41}/>
+      {/* <PropItUp lastName="Doe" firstName="Jane" age={67}/>
+      <PropItUp lastName="Miller" firstName="Frank" age={41}/> */}
+      <PersonCard firstName ='Chris' lastName='Frederick' age='41' hairColor='Brown'/>
+      <PersonCard firstName ='Nysha' lastName='Frederick' age='29' hairColor='Brown'/>
+      <PersonCard firstName ='Harry' lastName='Frederick' age='1' hairColor='Blonde'/>
     </div>
   );
 }
